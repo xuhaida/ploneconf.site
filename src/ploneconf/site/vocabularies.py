b2ee01f2eb0da4ee12960648d0316f75b0dc472d
Philip Bauer <bauer@starzel.de>
2018-12-01 09:14:18 +0100

    remove obsolete vocabulary

--- src/ploneconf/site/vocabularies.py
+++ src/ploneconf/site/vocabularies.py
@@ -1,24 +1,9 @@
 # -*- coding: utf-8 -*-
 from plone import api
-from plone.i18n.normalizer.interfaces import IIDNormalizer
-from zope.component import queryUtility
 from plone.app.vocabularies.terms import safe_simplevocabulary_from_values
 from zope.interface import implementer
-from zope.schema.interfaces import IVocabularyFactory
-from zope.schema.vocabulary import SimpleVocabulary
 from zope.schema.interfaces import IContextSourceBinder
 
-
-@implementer(IVocabularyFactory)
-class RoomsVocabularyFactory(object):
-
-    def __call__(self, context):
-        values = api.portal.get_registry_record('ploneconf.rooms')
-        normalizer = queryUtility(IIDNormalizer)
-        items = [(normalizer.normalize(i), i) for i in values]
-        return SimpleVocabulary.fromItems(items)
-
-RoomsVocabulary = RoomsVocabularyFactory()
 
 @implementer(IContextSourceBinder)
 class RegistryValueVocabulary(object):

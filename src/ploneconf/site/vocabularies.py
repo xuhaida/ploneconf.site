d4c2e5ca7860c5aea849e6c9d8bb179e95b6fad1
Philip Bauer <bauer@starzel.de>
2018-12-01 09:08:15 +0100

    add RegistryValueVocabulary

--- src/ploneconf/site/vocabularies.py
+++ src/ploneconf/site/vocabularies.py
@@ -2,9 +2,11 @@
 from plone import api
 from plone.i18n.normalizer.interfaces import IIDNormalizer
 from zope.component import queryUtility
+from plone.app.vocabularies.terms import safe_simplevocabulary_from_values
 from zope.interface import implementer
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
+from zope.schema.interfaces import IContextSourceBinder
 
 
 @implementer(IVocabularyFactory)
@@ -17,3 +19,13 @@ class RoomsVocabularyFactory(object):
         return SimpleVocabulary.fromItems(items)
 
 RoomsVocabulary = RoomsVocabularyFactory()
+
+@implementer(IContextSourceBinder)
+class RegistryValueVocabulary(object):
+
+    def __init__(self, value_name):
+        self.value_name = value_name
+
+    def __call__(self, context):
+        values = api.portal.get_registry_record(self.value_name)
+        return safe_simplevocabulary_from_values(values)

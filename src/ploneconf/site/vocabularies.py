69b8360ad9fa28039ef4824a0b048f81adbe4199
Philip Bauer <bauer@starzel.de>
2019-01-29 14:09:33 +0100

    use less obscure way to get the values in the vocabularies

--- src/ploneconf/site/vocabularies.py
+++ src/ploneconf/site/vocabularies.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-from inspect import currentframe
 from plone import api
 from plone.app.vocabularies.terms import safe_simplevocabulary_from_values
 from zope.interface import provider
@@ -7,13 +6,21 @@ from zope.schema.interfaces import IVocabularyFactory
 
 
 @provider(IVocabularyFactory)
-def RegistryValueVocabularyFactory(context):
-    # get the name of this utility by inspecting the previous stack
-    name = currentframe().f_back.f_locals.get('name', None)
-    if name is None:
-        name = currentframe().f_back.f_locals.get('vocabulary', None)
-    if name is None:
-        return []
-    name = str(name)
+def RoomVocabulary(context):
+    name = 'ploneconf.room'
+    values = api.portal.get_registry_record(name)
+    return safe_simplevocabulary_from_values(values)
+
+
+@provider(IVocabularyFactory)
+def TalkTypeVocabulary(context):
+    name = 'ploneconf.type_of_talk'
+    values = api.portal.get_registry_record(name)
+    return safe_simplevocabulary_from_values(values)
+
+
+@provider(IVocabularyFactory)
+def AudienceVocabulary(context):
+    name = 'ploneconf.audience'
     values = api.portal.get_registry_record(name)
     return safe_simplevocabulary_from_values(values)
